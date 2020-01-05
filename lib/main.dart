da71f0ca1d4f9e0fb9eff913318f26e85ed3a349
Balu Babu <balub997@gmail.com>
2020-01-05 19:51:53 +0530

    fallback commit

--- lib/main.dart
+++ lib/main.dart
@@ -19,14 +19,7 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-      ],
+      children: <Widget>[Expande],
     );
   }
 }
